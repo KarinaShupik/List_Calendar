b2ed87b9df5ee186763e1ef45951b39ac84b0e1b
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-03-12 16:05:51 +0200

    List_Calendar

---
+++ List_Calendar/Calendar.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Calendar
+{
+    internal class Calendar
+    {
+        private Node head;
+        private int size;
+
+        public Calendar()
+        {
+            head = null;
+            size = 0;
+        }
+
+        private class Node
+        {
+            public Date Date { get; set; }
+            public Node Next { get; set; }
+
+            public Node(Date date)
+            {
+                Date = date;
+                Next = null;
+            }
+        }
+
+        public void Add(Date date)
+        {
+            Node node = new Node(date);
+
+            if (head == null)
+            {
+                head = node;
+                node.Next = head;
+            }
+            else
+            {
+                Node current = head;
+
+                while (current.Next != head)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = node;
+                node.Next = head;
+            }
+
+            size++;
+        }
+
+        public void Remove(Date date)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            Node current = head;
+            Node previous = null;
+
+            do
+            {
+                if (current.Date.Equals(date))
+                {
+                    if (previous == null)
+                    {
+                        head = current.Next;
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                    }
+
+                    size--;
+                    return;
+                }
+
+                previous = current;
+                current = current.Next;
+            } while (current != head);
+        }
+
+        public List<Date> GetDatesForMonth(int month, int year)
+        {
+            List<Date> dates = new List<Date>();
+
+            if (head == null)
+            {
+                return dates;
+            }
+
+            Node current = head;
+
+            do
+            {
+                if (current.Date.Month == month && current.Date.Year == year)
+                {
+                    dates.Add(current.Date);
+                }
+
+                current = current.Next;
+            } while (current != head);
+
+            return dates;
+        }
+    }
+}
---
+++ List_Calendar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Calendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendar calendar = new Calendar();
+
+            // Додавання дат
+            calendar.Add(new Date(1, 1, 2022));
+            calendar.Add(new Date(2, 1, 2022));
+            calendar.Add(new Date(3, 1, 2022));
+            calendar.Add(new Date(1, 2, 2022));
+            calendar.Add(new Date(14, 2, 2022));
+            calendar.Add(new Date(1, 3, 2022));
+
+            // Виведення дат для певного місяця та року
+            List<Date> january2022Dates = calendar.GetDatesForMonth(1, 2022);
+            Console.WriteLine("January 2022 dates:");
+            foreach (Date date in january2022Dates)
+            {
+                Console.WriteLine(date);
+            }
+
+            // Видалення дати
+            calendar.Remove(new Date(1, 3, 2022));
+
+            // Виведення дат після видалення
+            List<Date> march2022Dates = calendar.GetDatesForMonth(3, 2022);
+            Console.WriteLine("March 2022 dates:");
+            foreach (Date date in march2022Dates)
+            {
+                Console.WriteLine(date);
+            }
+        }
+    }
+}
